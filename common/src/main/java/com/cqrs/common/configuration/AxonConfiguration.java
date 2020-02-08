815155492e6499ecfcaa629e3c805308777d057f
geonyeong.kim <c2619zz@naver.com>
2020-02-09 07:15:14 +0900

    axon metric dependency add, monitor class register

--- common/src/main/java/com/cqrs/common/configuration/AxonConfiguration.java
+++ common/src/main/java/com/cqrs/common/configuration/AxonConfiguration.java
@@ -1,15 +1,21 @@
 package com.cqrs.common.configuration;
 
+import com.codahale.metrics.MetricRegistry;
 import com.cqrs.common.datasource.MongoConfig;
 import com.cqrs.common.datasource.MongoDataSource;
 import com.cqrs.common.interceptor.EventLoggingDispatchInterceptor;
 import com.cqrs.common.interceptor.MyEventHandlerInterceptor;
 import com.mongodb.MongoClient;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.config.Configurer;
+import org.axonframework.config.ConfigurerModule;
+import org.axonframework.config.MessageMonitorFactory;
 import org.axonframework.eventhandling.EventBus;
 import org.axonframework.eventhandling.TrackedEventMessage;
+import org.axonframework.eventhandling.TrackingEventProcessor;
 import org.axonframework.eventhandling.TrackingEventProcessorConfiguration;
 import org.axonframework.eventhandling.tokenstore.TokenStore;
 import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
@@ -19,7 +25,13 @@ import org.axonframework.extensions.mongo.DefaultMongoTemplate;
 import org.axonframework.extensions.mongo.eventhandling.saga.repository.MongoSagaStore;
 import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoEventStorageEngine;
 import org.axonframework.extensions.mongo.eventsourcing.tokenstore.MongoTokenStore;
+import org.axonframework.messaging.Message;
 import org.axonframework.messaging.StreamableMessageSource;
+import org.axonframework.metrics.CapacityMonitor;
+import org.axonframework.metrics.MessageCountingMonitor;
+import org.axonframework.metrics.MessageTimerMonitor;
+import org.axonframework.metrics.PayloadTypeMessageMonitorWrapper;
+import org.axonframework.monitoring.MultiMessageMonitor;
 import org.axonframework.serialization.Serializer;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -66,6 +78,48 @@ public class AxonConfiguration implements InitializingBean {
                 .mongoTemplate(DefaultMongoTemplate.builder().mongoDatabase(mongoClient).build())
                 .serializer(serializer)
                 .build();
+    }
+
+    @Bean
+    public ConfigurerModule metricConfigurer(MetricRegistry metricRegistry){
+        return configurer -> {
+            instrumentEventProcessors(metricRegistry, configurer);
+            instrumentCommandBus(metricRegistry, configurer);
+        };
+    }
+
+    private void instrumentEventProcessors(MetricRegistry metricRegistry, Configurer configurer) {
+
+        PayloadTypeMessageMonitorWrapper<MessageCountingMonitor> messageCounterPerType =
+                new PayloadTypeMessageMonitorWrapper<>(MessageCountingMonitor::new);
+        PayloadTypeMessageMonitorWrapper<MessageTimerMonitor> messageTimerPerType =
+                new PayloadTypeMessageMonitorWrapper<>(MessageTimerMonitor::new);
+        MultiMessageMonitor<Message<?>> multiMessageMonitor =
+                new MultiMessageMonitor<>(messageCounterPerType, messageTimerPerType);
+        configurer.configureMessageMonitor(EventBus.class, configuration -> multiMessageMonitor);
+
+        MetricRegistry eventBusRegistry = new MetricRegistry();
+        eventBusRegistry.register("messageCounterPerType", messageCounterPerType);
+        eventBusRegistry.register("messageTimerPerType", messageTimerPerType);
+        metricRegistry.register("eventBus", eventBusRegistry);
+    }
+
+    private void instrumentCommandBus(MetricRegistry metricRegistry, Configurer configurer) {
+        PayloadTypeMessageMonitorWrapper<MessageCountingMonitor> messageCounterPerType =
+                new PayloadTypeMessageMonitorWrapper<>(MessageCountingMonitor::new);
+        PayloadTypeMessageMonitorWrapper<MessageTimerMonitor> messageTimerPerType =
+                new PayloadTypeMessageMonitorWrapper<>(MessageTimerMonitor::new);
+        PayloadTypeMessageMonitorWrapper <CapacityMonitor> capacityMonitor =
+                new PayloadTypeMessageMonitorWrapper<>(CapacityMonitor::new);
+
+        MultiMessageMonitor<Message<?>> multiMessageMonitor =
+                new MultiMessageMonitor<>(messageCounterPerType, messageTimerPerType, capacityMonitor);
+        configurer.configureMessageMonitor(CommandBus.class, configuration -> multiMessageMonitor);
+
+        MetricRegistry commandBusRegistry = new MetricRegistry();
+        commandBusRegistry.register("messageCounterPerType", messageCounterPerType);
+        commandBusRegistry.register("messageTimerPerType", messageTimerPerType);
+        metricRegistry.register("commandBus", commandBusRegistry);
     }
 
     @Override
